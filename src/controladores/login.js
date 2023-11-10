4bb2174f76deee70cc4b2b0464cc1b3a9afb19c5
Andressa Mega <andressamega@souunisuam.com.br>
2023-11-10 12:45:55 -0300

    EndPoint Login criado

--- src/controladores/login.js
+++ src/controladores/login.js
@@ -1,3 +1,21 @@
+const usuario = {
+    email: 'and@email.com',
+    senha: '123',
+}
+
 const login = async (req, res) => {
+    const { email, senha} = req.body;
+
+    if (usuario.email !== email) {
+        return res.status(400).json({ message: 'email ou senha inválidas'})
+    }
+    if (usuario.senha !== senha) {
+        return res.status(400).json({ message: 'email ou senha inválidas'})
+    }
+
     return res.json({ message: 'Login efetuado com sucesso'})
 };
+
+module.exports = {
+    login
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 require('dotenv').config()
 const express = require('express')
+const { login } = require('./controladores/login')
 
 const app = express()
 
 app.use(express.json())
 
-app.post('/login', )
+app.post('/login', login)
 
 app.listen(process.env.PORT)
